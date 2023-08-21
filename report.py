4600b1c4d8e9974c70ee7618355e3ba79ade402b
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-08-21 06:00:39 +0200

    Add more complete CSV data

--- report.py
+++ report.py
@@ -37,38 +37,38 @@ def calculate(df):
 
     print("Calculating stats...")
 
-    longest_stories = get_longest_stories(df)
+    stories_length = get_stories_length(df)
 
-    top_appearances = df.loc[df.appearances.str.len() != 0, "appearances"].value_counts()[:10]
-    top_individual_appearances = df.loc[df["appearances"] != "", "appearances"].str.split(", ").explode().value_counts()[:10]
-    top_writers = df.loc[df.writing.str.len() != 0, "writing"].value_counts()[:10]
-    top_artists = df.loc[df.art.str.len() != 0, "art"].value_counts()[:10]
-    top_years = df.loc[df["date"] != "", "date"].dt.strftime("%Y").value_counts()[:10]
+    appearances = df.loc[df.appearances.str.len() != 0, "appearances"].value_counts()
+    individual_appearances = df.loc[df["appearances"] != "", "appearances"].str.split(", ").explode().value_counts()
+    writers = df.loc[df.writing.str.len() != 0, "writing"].value_counts()
+    artists = df.loc[df.art.str.len() != 0, "art"].value_counts()
+    years = df.loc[df["date"] != "", "date"].dt.strftime("%Y").value_counts()
 
-    top_decades = get_decades(df)
+    decades = get_decades(df)
 
     data = {
-        "longest_stories": longest_stories,
-        "top_appearances": top_appearances,
-        "top_individual_appearances": top_individual_appearances,
-        "top_writers": top_writers,
-        "top_artists": top_artists,
-        "top_years": top_years,
-        "top_decades": top_decades
+        "stories_length": stories_length,
+        "appearances": appearances,
+        "individual_appearances": individual_appearances,
+        "writers": writers,
+        "artists": artists,
+        "years": years,
+        "decades": decades
     }
 
     return data
 
 
-def get_longest_stories(df):
+def get_stories_length(df):
 
     df = df[~df.pages.str.contains("/")].iloc[:, [1,2]] # some pages contain a slash, like 7 7/8
     df["pages"] = df["pages"].astype(int)
-    df = df.sort_values(by = ["pages"], ascending=False).head(10)
+    df = df.sort_values(by = ["pages"], ascending=False)
     df["title"] = df["title"].str.replace("$", "'$'") # replace dollar signs with the letter "s" (they are usually used as a wordplay with Uncle Scrooge and matplotlib interprets that symbol as math text, causing issues)
-    longest_stories = pd.Series(df["pages"].values, index=df["title"], name="pages")    # convert page number from str to int
+    stories_length = pd.Series(df["pages"].values, index=df["title"], name="pages")    # convert page number from str to int
 
-    return longest_stories
+    return stories_length
 
 
 def get_decades(df):
@@ -79,11 +79,11 @@ def get_decades(df):
         if str(year).isnumeric():
             decade = str(year)[:3] + "0"
             decades.append(decade)
-    top_decades = dict(sorted(Counter(decades).items(), key = lambda x: x[1], reverse=True))
-    top_decades = pd.Series(top_decades, name="count")
-    top_decades.index.name = "decade"
+    decades = dict(sorted(Counter(decades).items(), key = lambda x: x[1], reverse=True))
+    decades = pd.Series(decades, name="count")
+    decades.index.name = "decade"
 
-    return top_decades
+    return decades
 
 
 def write_data(data):
@@ -100,24 +100,26 @@ def draw_plots(data):
 
     for title in data:
 
+        data[title] = data[title].iloc[:10]
+
         # Barplot
         sns.set(rc={"figure.figsize":(20,8.27)})
-        plot = sns.barplot(x = data[title].values, y = data[title].index, orient = "h").set(title = title.title().replace('_', ' '), xlabel = None, ylabel = None)
+        plot = sns.barplot(x = data[title].values, y = data[title].index, orient = "h").set(title = "Top " + title.title().replace('_', ' '), xlabel = None, ylabel = None)
         plt.tight_layout()
-        plt.savefig(f"report/plots/{title}_bar_plot.png")
+        plt.savefig(f"report/plots/top_{title}_bar_plot.png")
 
         plt.figure(clear=True)
     
-        if title != "longest_stories":   # pie chart has no meaning for this stat
+        if title != "stories_length":   # pie chart has no meaning for this stat
             # Pie chart
             labels = data[title].index
             sizes = data[title].values / data[title].values.sum() * 100
             plt.pie(sizes, textprops = {"color":"w"})
             labels = [f"{l} - {s:0.1f}%" for l, s in zip(labels, sizes)]
             plt.legend(labels = labels, bbox_to_anchor = (1.6,1), loc = "best")
-            plt.title(title.title().replace('_', ' '))
+            plt.title("Top " + title.title().replace('_', ' '))
             plt.tight_layout()
-            plt.savefig(f"report/plots/{title}_pie_chart.png")
+            plt.savefig(f"report/plots/top_{title}_pie_chart.png")
 
             plt.figure(clear=True)
 
